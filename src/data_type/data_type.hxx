0226976f1f90180ef594bed1af62370b097a4b43
Huy Nguyen <huy55465@gmail.com>
2024-09-15 13:42:13 -0400

    feat: alias for data types
    So that everything looks more consistent.
    
    commit-by: Huy Nguyen

---
+++ src/data_type/data_type.hxx
@@ -0,0 +1,13 @@
+#ifndef SMOLDB_CPP_DATA_TYPE_HXX
+#define SMOLDB_CPP_DATA_TYPE_HXX
+
+#include <cstdint>
+#include <string>
+namespace smoldb {
+
+using UInt32 = std::uint32_t;
+using VarChar = std::string;
+
+}
+
+#endif // !SMOLDB_CPP_DATA_TYPE_HXX
