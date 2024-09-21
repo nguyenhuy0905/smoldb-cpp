7720979543d820da70abfdf2b2d685998298e130
Huy Nguyen <huy55465@gmail.com>
2024-09-21 10:34:48 -0400

    some light module testing

--- src/main.cxx
+++ src/main.cxx
@@ -1,1 +1,10 @@
-auto main() -> int { return 0; }
+#ifndef ENABLE_MODULE
+#include "table.hxx"
+#else
+import smoldb_cpp;
+#endif // !ENABLE_MODULE
+
+auto main() -> int {
+    smoldb::Table tab{};
+    return 0;
+}
